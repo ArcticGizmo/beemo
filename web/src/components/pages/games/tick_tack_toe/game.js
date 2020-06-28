ef65172b438f9e7d520a0343cf678528aa6e015a
ArcticGizmo <jon.howell314@gmail.com>
2020-06-28 12:06:32 +0800

    updated to 2D array

--- web/src/components/pages/games/tick_tack_toe/game.js
+++ web/src/components/pages/games/tick_tack_toe/game.js
@@ -3,7 +3,88 @@
 // https://phaser.io/examples/v3/view/game-objects/graphics/primitives/rectangle-gradient-filled
 import Phaser from 'phaser';
 
-const BLACK = '#000000';
+const BLACK = 0x000000;
+const RED = 0xff0000;
+
+function createEmptyCells() {
+  const grid = [];
+  for (let row = 0; row < 3; row++) {
+    const rowList = [];
+    for (let col = 0; col < 3; col++) {
+      rowList.push({});
+    }
+    grid.push(rowList);
+  }
+
+  return grid;
+}
+
+class GameState {
+  constructor(gridAreas) {
+    this._player = 0;
+    this._letters = ['X', 'O'];
+    this._cells = createEmptyCells();
+
+    // add game objects for each cell
+    gridAreas.forEach(row => {
+      row.forEach(cell => {
+        this._cells[cell.row][cell.col] = {
+          row: cell.row,
+          col: cell.col,
+          area: cell.area,
+          text: cell.text,
+          player: null,
+          letter: '',
+        };
+      });
+    });
+  }
+
+  updatePlacements() {
+    console.dir('--- updating board');
+    this._cells.forEach(row => {
+      row.forEach(col => {
+        col.text.setText(col.letter);
+      });
+    });
+  }
+
+  checkWinner() {}
+
+  setLetter(row, col, player) {
+    const cell = this._cells[row][col];
+
+    if (!cell) {
+      console.error(`[TTT] Invalid cell (${row}, ${col})`);
+      return;
+    }
+
+    if (player == null) {
+      console.error('[TTT] player not valid');
+      return;
+    }
+
+    if (this._player !== player) {
+      console.log('[TTT] Not the current players turn');
+      return;
+    }
+
+    if (cell.letter) {
+      console.log('[TTT] Cell already taken');
+      return;
+    }
+
+    const letter = this._letters[player];
+    cell.player = player;
+    cell.letter = letter;
+    this.switchPlayer();
+    this.updatePlacements();
+  }
+
+  switchPlayer() {
+    this._player = this._player === 0 ? 1 : 0;
+  }
+}
 
 export function launch(containerId, width = 500, height = 500) {
   const config = {
@@ -37,24 +118,9 @@ function create() {
   createGridLines.bind(this)(widthSpacing, heightSpacing, lineTickness);
 
   // create grid areas
-  createGridAreas.bind(this)(widthSpacing, heightSpacing, lineTickness);
+  const grid = createGridAreas.bind(this)(widthSpacing, heightSpacing);
 
-  // const circle = this.add.circle(200, 200, 80, 0x6666ff);
-
-  // vLineLeft.setInteractive();
-
-  // // use this for hover
-  // vLineLeft.on('pointerover', () => { console.log('pointerover'); });
-
-  //
-
-  // vLineLeft.input.useHandCursor = true;
-
-  //   graphics.inputEnabled = true;
-  // graphics.input.useHandCursor = true;
-  // graphics.events.onInputUp.add(onClick, this);
-
-  // create the clickable regions (change color on click)
+  this.$state = new GameState(grid);
 }
 
 function createBackground() {
@@ -87,29 +153,45 @@ function createGridLines(widthSpacing, heightSpacing, tickness) {
   return [vLineLeft, vLineRight, hLineTop, hLineBottom];
 }
 
-function createGridAreas(widthSpacing, heightSpacing, thickness) {
+function createGridAreas(widthSpacing, heightSpacing) {
   const grid = [];
-  for (let i = 0; i < 3; i++) {
-    const row = [];
-    for (let j = 0; j < 3; j++) {
+  const colOffset = widthSpacing / 2;
+  const rowOffset = heightSpacing / 2;
+  for (let row = 0; row < 3; row++) {
+    const rowList = [];
+
+    for (let col = 0; col < 3; col++) {
+      const rect = this.add
+        .rectangle(
+          widthSpacing * col + colOffset,
+          heightSpacing * row + rowOffset,
+          widthSpacing,
+          heightSpacing,
+          null,
+          0,
+        )
+        .setInteractive()
+        .on('pointerdown', () => {
+          this.$state.setLetter(row, col, this.$state._player);
+        });
+
       var style = {
         font: `bold ${heightSpacing}px Arial`,
         fill: '#fff',
       };
 
       const text = this.add
-        .text(widthSpacing * (i + 0.5), heightSpacing * (j + 0.5), 'X', style)
-        .setOrigin(0.5)
-        .setInteractive()
-        .on('pointerover', function() {
-          this.setTint('#ababab');
-        })
-        .on('pointerout', function() {
-          this.clearTint();
-        });
-      row.push(text);
+        .text(widthSpacing * (col + 0.5), heightSpacing * (row + 0.5), '', style)
+        .setOrigin(0.5);
+
+      rowList.push({
+        row,
+        col,
+        area: rect,
+        text,
+      });
     }
-    grid.push(row);
+    grid.push(rowList);
   }
 
   return grid;
