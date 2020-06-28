d0145b3263d5e9f3c01ef45d938cbfcca0946fee
ArcticGizmo <jon.howell314@gmail.com>
2020-06-28 10:56:24 +0800

    text and hover areas

--- web/src/components/pages/games/tick_tack_toe/game.js
+++ web/src/components/pages/games/tick_tack_toe/game.js
@@ -1,5 +1,6 @@
 // https://phaser.io/examples/v3/view/input/mouse/circle-hit-area
 // https://phaser.io/examples/v3/view/game-objects/shapes/circle
+// https://phaser.io/examples/v3/view/game-objects/graphics/primitives/rectangle-gradient-filled
 import Phaser from 'phaser';
 
 const BLACK = '#000000';
@@ -25,49 +26,91 @@ function preload() {
 }
 
 function create() {
-  // create a background graphic
-  const background = this.add.graphics();
-  background.fillGradientStyle(0xff0000, 0xff0000, 0xffff00, 0xffff00, 1);
-  background.fillRect(0, 0, this.width, this.height);
-
-  // create the bars to separate the game area
   const widthSpacing = this.width / 3;
   const heightSpacing = this.height / 3;
-  const barThickness = 5;
+  const lineTickness = 5;
+
+  // create a background graphic
+  createBackground.bind(this)();
+
+  // create the bars to separate the game area
+  createGridLines.bind(this)(widthSpacing, heightSpacing, lineTickness);
+
+  // create grid areas
+  createGridAreas.bind(this)(widthSpacing, heightSpacing, lineTickness);
 
   // const circle = this.add.circle(200, 200, 80, 0x6666ff);
 
-  const vLineLeft = this.add.rectangle(
-    widthSpacing,
-    this.height / 2,
-    barThickness,
-    this.height,
-    BLACK,
-  );
+  // vLineLeft.setInteractive();
+
+  // // use this for hover
+  // vLineLeft.on('pointerover', () => { console.log('pointerover'); });
+
+  //
+
+  // vLineLeft.input.useHandCursor = true;
+
+  //   graphics.inputEnabled = true;
+  // graphics.input.useHandCursor = true;
+  // graphics.events.onInputUp.add(onClick, this);
+
+  // create the clickable regions (change color on click)
+}
+
+function createBackground() {
+  const background = this.add.graphics();
+  background.fillGradientStyle(0xff0000, 0xff0000, 0xffff00, 0xffff00, 1);
+  background.fillRect(0, 0, this.width, this.height);
+}
+
+function createGridLines(widthSpacing, heightSpacing, tickness) {
+  const vLineLeft = this.add.rectangle(widthSpacing, this.height / 2, tickness, this.height, BLACK);
 
   const vLineRight = this.add.rectangle(
     2 * widthSpacing,
     this.height / 2,
-    barThickness,
+    tickness,
     this.height,
     BLACK,
   );
 
-  const hLineTop = this.add.rectangle(
-    this.width / 2,
-    heightSpacing,
-    this.width,
-    barThickness,
-    BLACK,
-  )
+  const hLineTop = this.add.rectangle(this.width / 2, heightSpacing, this.width, tickness, BLACK);
 
   const hLineBottom = this.add.rectangle(
     this.width / 2,
     2 * heightSpacing,
     this.width,
-    barThickness,
+    tickness,
     BLACK,
-  )
+  );
 
-  // create the clickable regions (change color on click)
+  return [vLineLeft, vLineRight, hLineTop, hLineBottom];
+}
+
+function createGridAreas(widthSpacing, heightSpacing, thickness) {
+  const grid = [];
+  for (let i = 0; i < 3; i++) {
+    const row = [];
+    for (let j = 0; j < 3; j++) {
+      var style = {
+        font: `bold ${heightSpacing}px Arial`,
+        fill: '#fff',
+      };
+
+      const text = this.add
+        .text(widthSpacing * (i + 0.5), heightSpacing * (j + 0.5), 'X', style)
+        .setOrigin(0.5)
+        .setInteractive()
+        .on('pointerover', function() {
+          this.setTint('#ababab');
+        })
+        .on('pointerout', function() {
+          this.clearTint();
+        });
+      row.push(text);
+    }
+    grid.push(row);
+  }
+
+  return grid;
 }
